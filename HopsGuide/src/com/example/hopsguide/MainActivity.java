e73295af2d8d9e832dfc21ac34f67c1569b440b6
Greger Siem Knudsen <gkn036@student.uib.no>
2014-09-12 15:20:20 +0200

    Fjernet ubrukt import

--- HopsGuide/src/com/example/hopsguide/MainActivity.java
+++ HopsGuide/src/com/example/hopsguide/MainActivity.java
@@ -7,8 +7,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.view.*;
-
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 	
