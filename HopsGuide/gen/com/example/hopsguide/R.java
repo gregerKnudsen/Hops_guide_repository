6029ed5b7901f1548a66fa9feb7724839b128da6
Greger Siem Knudsen <gkn036@student.uib.no>
2014-09-15 10:31:49 +0200

    Velkomstskjerm sitt grunnlag er ferdig

--- HopsGuide/gen/com/example/hopsguide/R.java
+++ HopsGuide/gen/com/example/hopsguide/R.java
@@ -1079,6 +1079,7 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int app_icon=0x7f020057;
         public static final int ic_launcher=0x7f020058;
+        public static final int informasjonsikon=0x7f020059;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1096,8 +1097,8 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f05003f;
-        public static final int button2=0x7f050040;
+        public static final int button1=0x7f05003e;
+        public static final int button2=0x7f05003f;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1116,6 +1117,7 @@ containing a value of this type.
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
         public static final int imageView1=0x7f05003c;
+        public static final int imageView2=0x7f050040;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1143,7 +1145,6 @@ containing a value of this type.
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
         public static final int textView1=0x7f05003d;
-        public static final int textView2=0x7f05003e;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1257,6 +1258,7 @@ containing a value of this type.
         public static final int browseButtonLabel=0x7f0a0016;
         public static final int hello_world=0x7f0a0018;
         public static final int information=0x7f0a0012;
+        public static final int informationButtonDescription=0x7f0a001e;
         public static final int resultTextArea=0x7f0a001d;
         public static final int search=0x7f0a0011;
         public static final int searchBoxDescription=0x7f0a001a;
