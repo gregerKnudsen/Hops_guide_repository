5bd2cbd263cf86f7c4ecee9f4b37d123cd01ef64
Greger Siem Knudsen <gkn036@student.uib.no>
2014-09-15 10:49:40 +0200

    Fjernet utropstegnvarsel på prosjekt

--- HopsGuide/src/com/example/hopsguide/SearchActivity.java
+++ HopsGuide/src/com/example/hopsguide/SearchActivity.java
@@ -1,8 +1,6 @@
 package com.example.hopsguide;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import android.support.v7.app.ActionBarActivity;
