6fccb0caf4f8ca1f9a6d91541f862f89b02a2a79
Greger Siem Knudsen <gkn036@student.uib.no>
2014-09-24 12:37:50 +0200

    Fortsetter med SQLite

---
+++ HopsGuide/src/com/example/hopsguide/SQLQueryFactory.java
@@ -0,0 +1,16 @@
+package com.example.hopsguide;
+
+import java.util.List;
+
+public class SQLQueryFactory {
+	
+	public static String selectHopsNames(){
+		return "SELECT Name FROM Hops";
+	}
+	
+	public static String insertHops(String _id, String country, float alpha, float beta, String type, 
+			int storageIndex, String typicalFor, String substitutes, String aroma, String information){
+		return "INSERT INTO Hops (_id, country, alpha, beta, type, storageIndex, typicalFor, substitutes, aroma, information) " +
+			   "VALUES (,?,?,?,?,?,?,?,?,?)";
+	}
+}
