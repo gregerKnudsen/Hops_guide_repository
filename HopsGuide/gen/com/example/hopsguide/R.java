cea2d011bb6d094593b7f11564f08bce44c9f4c5
Sindre <sin_eid@hotmail.com>
2014-10-09 13:19:35 +0200

    Information xml and pictures res updated.

--- HopsGuide/gen/com/example/hopsguide/R.java
+++ HopsGuide/gen/com/example/hopsguide/R.java
@@ -1080,7 +1080,8 @@ containing a value of this type.
         public static final int app_icon=0x7f020057;
         public static final int app_icon_v2=0x7f020058;
         public static final int ic_launcher=0x7f020059;
-        public static final int informasjonsikon=0x7f02005a;
+        public static final int info1=0x7f02005a;
+        public static final int informasjonsikon=0x7f02005b;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1094,11 +1095,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050047;
+        public static final int action_settings=0x7f050048;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int browseButton=0x7f050040;
+        public static final int browseButton=0x7f050041;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1114,24 +1115,24 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imageView1=0x7f05003d;
-        public static final int imgLogo=0x7f050042;
-        public static final int informationButton=0x7f05003f;
-        public static final int inputTextBox=0x7f050043;
+        public static final int imageView1=0x7f05003c;
+        public static final int imgLogo=0x7f050043;
+        public static final int informationButton=0x7f050040;
+        public static final int inputTextBox=0x7f050044;
         public static final int listMode=0x7f050001;
-        public static final int listView1=0x7f050045;
-        public static final int listView2=0x7f050046;
-        public static final int listViewResult=0x7f05003c;
+        public static final int listView1=0x7f050046;
+        public static final int listView2=0x7f050047;
+        public static final int listViewResult=0x7f05003f;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
-        public static final int myListsButton=0x7f050041;
+        public static final int myListsButton=0x7f050042;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
-        public static final int searchButton=0x7f050044;
+        public static final int searchButton=0x7f050045;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1149,7 +1150,8 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003e;
+        public static final int textView1=0x7f05003d;
+        public static final int textView2=0x7f05003e;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1272,6 +1274,8 @@ containing a value of this type.
         public static final int information=0x7f0a0011;
         public static final int informationButtonDescription=0x7f0a001b;
         public static final int informationContent=0x7f0a001d;
+        public static final int informationMain1=0x7f0a0023;
+        public static final int informationMain2=0x7f0a0024;
         public static final int listButtonDescription=0x7f0a0013;
         public static final int listButtonLabel=0x7f0a001f;
         public static final int listNames=0x7f0a0021;
@@ -2663,7 +2667,7 @@ containing a value of this type.
          */
         public static final int[] MenuView = {
             0x010100ae, 0x0101012c, 0x0101012d, 0x0101012e,
-            0x0101012f, 0x01010130, 0x01010131, 0x01010435
+            0x0101012f, 0x01010130, 0x01010131, 0x01010438
         };
         /**
           <p>
